14038edbc5687d515576424cf5a38ed1927fc856
Manish-ui-star <manishjaiswal10590@gmail.com>
2022-11-15 08:38:54 +0530

    WebApp

---
+++ MyFirstWebApp/src/test/java/com/manish/java/test/TestEmployee.java
@@ -0,0 +1,14 @@
+package com.manish.java.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestEmployee {
+    @Test
+	public void testEmp()
+	{
+		assertEquals("Hello", "Hello");
+	}
+	
+}
